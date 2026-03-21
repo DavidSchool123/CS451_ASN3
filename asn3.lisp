87710f17d2a24f588f6000e9a70e5de92ded7a81
David Ogunbanjo <85774740+DeReals@users.noreply.github.com>
2026-03-21 15:23:32 -0400

    Finalized the file structure

--- asn3.lisp
+++ asn3.lisp
@@ -3,3 +3,21 @@ Authors: David Ogunbanjo & Denis Moroz
 Description: This file contains the answers to the ASN3 assignment questions.
 Date: 3/21/2026
 |#
+
+;; Question 1
+
+#| Calculate the Discrete Fourier Transform on a given list of real numbers. The only parameter 
+this function is going to receive is a list of real numbers. Moreover, this function must return another 
+list of (mostly) complex numbers from the result of the DFT formula. 
+(LLM Euler's Formula, cos and sin) |#
+
+
+;; Question 2
+
+#| Calculate the Inverse Discrete Fourier Transform on a given list of (mostly) complex numbers. 
+The only parameter to this function is a list of numbers representing data in the frequency domain.
+(LLM must use Euler's Formula, cos and sin)
+Moreover, this function must return another list of real numbers from the result of the IDFT formula. |#
+(print "hello")
+
+
