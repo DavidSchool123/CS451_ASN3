38dda64b575cc60e5812b885a98294f5ef16993b
David Ogunbanjo <85774740+DeReals@users.noreply.github.com>
2026-03-21 17:31:34 -0400

    Made a base to work with for question 1

--- asn3.lisp
+++ asn3.lisp
@@ -11,13 +11,61 @@ this function is going to receive is a list of real numbers. Moreover, this func
 list of (mostly) complex numbers from the result of the DFT formula. 
 (LLM Euler's Formula, cos and sin) |#
 
+(defun DiscreteTransform (numbers)
+    (print numbers)
+    (let ((f_u_list '()) (N (length numbers)) (i (sqrt -1)))  ;; f(u) store the return list, N: length of the list, i: imaginary numbers
+        (let ((u 0))  ;; create the variable u for the outer loop
+            (do    ;; this is for the calculation of f(u) list
+                ()  ;; nothing
+                ((= u N) f_u_list)  ;; end cond
+                (progn  ;; outer loop body
+                    (print u) 
+                    
+                    (let ((x 0))
+                        (do  ;; inner loop to calculate the each summation
+                            ()
+                            ((= x N) "Done")
+                            (progn ;; inner loop body
+                                ;; perform the calculations
+                                (let (A B C D E F)  ;; declare the variable before initalizing it
+                                    (setq A (* -2 i pi u x)) 
+                                    (setq B (/ A N))
+                                    (setq C (exp B))
+                                    (setq D (* (nth x numbers) C))
+                                    (setq E (/ 1 N))
+                                    (setq F (* D E))
+
+
+                                    (push F f_u_list)  ;; add the result to the f(u) list
+                                )
+                                
+                                ;; done next flow
+                                (setq x (+ x 1))
+                            )
+                            
+                        )
+                    )
+
+                    ;; done next flow
+                    (setq u (+ u 1))    
+                )
+            )
+        )
+    )
+    
+
+)
+(DiscreteTransform '(1 2 3 4 5))
+
+
+#|            
+ |#
 
 ;; Question 2
 
 #| Calculate the Inverse Discrete Fourier Transform on a given list of (mostly) complex numbers. 
 The only parameter to this function is a list of numbers representing data in the frequency domain.
-(LLM must use Euler's Formula, cos and sin)
-Moreover, this function must return another list of real numbers from the result of the IDFT formula. |#
-(print "hello")
+(LLM must use Euler's Formula, cos and sin)|#
+
 
 
